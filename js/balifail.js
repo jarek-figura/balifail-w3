55bda40487c031ac9a3f6e9a03e9245880d512ca
Jarek Figura <jarek.figura@icloud.com>
2018-02-18 23:56:52 +0100

    Baifail Whippets german site created in w3schools w3.css framework. It is a preview of possibilities of the w3.css framework.

---
+++ js/balifail.js
@@ -0,0 +1,41 @@
+// M E N U
+function hideSmall() {
+	var x = document.getElementById("hide");
+	if (x.className.indexOf("w3-show") === -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+function dropMenu1() {
+	var x = document.getElementById("drop1");
+	if (x.className.indexOf("w3-show") === -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+function dropMenu2() {
+	var x = document.getElementById("drop2");
+	if (x.className.indexOf("w3-show") === -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+function dropMenu3() {
+	var x = document.getElementById("drop3");
+	if (x.className.indexOf("w3-show") === -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+function dropMenu4() {
+	var x = document.getElementById("drop4");
+	if (x.className.indexOf("w3-show") === -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
